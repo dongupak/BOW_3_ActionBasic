3eb796b8273349ebc7649f3ed2b507e63d0f472e
dongupak <dongupak@bitbucket.org>
2014-05-15 19:58:56 +0900

    first commit

---
+++ Action_Basic/GameLayer.h
@@ -0,0 +1,16 @@
+//
+//  GameLayer.h
+//  Action_Basic
+//
+//  Created by ivis on 13. 6. 26..
+//  Copyright 2013년 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface GameLayer : CCLayer {
+    
+}
+
+@end
---
+++ Action_Basic/GameLayer.m
@@ -0,0 +1,14 @@
+//
+//  GameLayer.m
+//  Action_Basic
+//
+//  Created by ivis on 13. 6. 26..
+//  Copyright 2013년 __MyCompanyName__. All rights reserved.
+//
+
+#import "GameLayer.h"
+
+
+@implementation GameLayer
+
+@end
